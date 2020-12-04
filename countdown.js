4a3841f71ac7f510ca3c5da2a8652998ccd24ba7
Eirol <74545840+eirol@users.noreply.github.com>
2020-12-04 11:25:22 +0100

    Less and more phone madness

--- countdown.js
+++ countdown.js
@@ -1,26 +1,30 @@
 (function () {
   function pluralize(element, value, singular, plural) {
-      // Taken from https://stackoverflow.com/a/118251
+    /*
+    // Taken from https://stackoverflow.com/a/118251
 
-      // Roughly
+    // Roughly
 
-      var test = document.getElementById("test");
+    var test = document.getElementById("test");
 
-      test.innerText = singular;
-      var a = test.clientWidth;
+    test.innerText = singular;
+    var a = test.clientWidth;
 
-      test.innerText = plural;
-      var b = test.clientWidth;
+    test.innerText = plural;
+    var b = test.clientWidth;
 
-      if (value == 1) {
-        element.style.marginLeft = ((b-a)/2) + "px";
-        element.style.marginRight = ((b-a)/2) + "px";
-      } else {
-        element.style.marginLeft = "0px";
-        element.style.marginRight = "0px";
-      }
+    // I guess we could have used a min width or something, but whatever (that's exactly what I've done)
 
-      element.innerText = (value == 1) ? singular : plural;
+    if (value == 1) {
+      element.style.marginLeft = ((b-a)/2) + "px";
+      element.style.marginRight = ((b-a)/2) + "px";
+    } else {
+      element.style.marginLeft = "0px";
+      element.style.marginRight = "0px";
+    }
+    */
+
+    element.innerText = (value == 1) ? singular : plural;
   }
 
   const second = 1000;
@@ -28,7 +32,7 @@
   const hour = minute * 60;
   const day = hour * 24;
 
-  let birthday = "Dec 7, 2020 12:00:00";
+  let birthday = "Dec 7, 2020 13:00:00";
   let countDown = new Date(birthday).getTime();
 
   let offset = 0;
@@ -46,8 +50,8 @@
     //document.body.style.backgroundPosition = x + "% " + y + "%";
 
     // For testing purposes
-    //offset += 100000;
-    //distance -= offset;
+    offset += 1000000;
+    distance -= offset;
 
     if (distance < 0) distance = 0;
 
@@ -60,6 +64,11 @@
     document.getElementById("hours").innerText = hoursLeft;
     document.getElementById("minutes").innerText = minutesLeft;
     document.getElementById("seconds").innerText = secondsLeft;
+
+    //daysLeft = (daysLeft % 2) ? 0 : 1;
+    hoursLeft = (hoursLeft % 2) ? 0 : 1;
+    minutesLeft = (minutesLeft % 2) ? 0 : 1;
+    secondsLeft = (secondsLeft % 2) ? 0 : 1;
 
     pluralize(document.getElementById("daylabel"), daysLeft, "Día", "Días");
     pluralize(document.getElementById("hourlabel"), hoursLeft, "Hora", "Horas");
